b74f4b3d26225e10140e0c3cb8bb53073299ebb2
Tom Schoutens <tom.schoutens@simbuka.com>
2023-12-04 11:42:50 +0100

    Finish day 4

---
+++ 2023/day4/AdventOfCode2023Day4Part1.java
@@ -0,0 +1,100 @@
+package nl.schoutens.adventofcode;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * Question: What is the sum of all of the scratchcard points?
+ */
+public class AdventOfCode2023Day4Part1 {
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "input.txt";
+        int sumOfScratchCardPoints = calculateSumOfScratchCardPoints(fileName, StandardCharsets.UTF_8);
+        System.out.println("Sum of all of the scratchcard points: " + sumOfScratchCardPoints);
+    }
+
+    private static int calculateSumOfScratchCardPoints(String fileName, Charset cs) throws IOException {
+        int sumOfScratchCardPoints = 0;
+        File file = new File(fileName);
+        FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis, cs);
+        BufferedReader br = new BufferedReader(isr);
+        String line;
+
+        while((line = br.readLine()) != null){
+            sumOfScratchCardPoints += calculateScratchCardPoints(line);
+        }
+        br.close();
+
+        return sumOfScratchCardPoints;
+    }
+
+    /*
+     * Example input: 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+     */
+    private static int calculateScratchCardPoints(String scratchCardLine) {
+        /*
+         * Results in:
+         * 'Card 1'
+         * ' 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+         */
+        String[] scratchCardLineSplitOnColon = scratchCardLine.split(":");
+        /*
+         * Results in:
+         * ' 41 48 83 86 17 '
+         * ' 83 86  6 31 17  9 48 53'
+         */
+        String[] scratchCardLineSplitOnPipe = scratchCardLineSplitOnColon[1].split("\\|");
+
+        Integer[] winningNumbers = convertToNumberArray(scratchCardLineSplitOnPipe[0]);
+        Integer[] allNumbers = convertToNumberArray(scratchCardLineSplitOnPipe[1]);
+
+        HashSet<Integer> set = new HashSet<>();
+        set.addAll(Arrays.asList(winningNumbers));
+        set.retainAll(Arrays.asList(allNumbers));
+
+        int scratchCardPoints = getScratchCardPoints(set.size());
+        return scratchCardPoints;
+    }
+
+    /*
+     * Example input: ' 83 86  6 31 17  9 48 53'
+     */
+    private static Integer[] convertToNumberArray(String numbers) {
+        // use trim to remove spaces on beginning/end, replace duplicate space in between
+        String [] str = numbers.trim().replaceAll("  ", " ").split(" ");
+        int size = str.length;
+        Integer[] arr = new Integer[size];
+        for(int i=0; i<size; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        return arr;
+    }
+
+    private static int getScratchCardPoints(int numberOfWinningNumbers) {
+        if (numberOfWinningNumbers > 1) {
+            int points = 1;
+            for (int i=0; i<numberOfWinningNumbers-1; i++) {
+                points *= 2;
+            }
+            return points;
+        }
+        if (numberOfWinningNumbers == 1) {
+            return 1;
+        }
+
+        return 0;
+    }
+}
---
+++ 2023/day4/AdventOfCode2023Day4Part2.java
@@ -0,0 +1,104 @@
+package nl.schoutens.adventofcode;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * Question: What is the total number of scratchcards?
+ */
+public class AdventOfCode2023Day4Part2 {
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "input.txt";
+        int totalNumberOfScratchCards = calculateTotalNumberOfScratchCards(fileName, StandardCharsets.UTF_8);
+        System.out.println("Total number of scratchcards: " + totalNumberOfScratchCards);
+    }
+
+    private static int calculateTotalNumberOfScratchCards(String fileName, Charset cs) throws IOException {
+        int totalNumberOfScratchCards = 0;
+        int currentGameNumber = 1;
+        HashMap<Integer, Integer> copiedScratchCards = new HashMap<Integer, Integer>();
+        File file = new File(fileName);
+        FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis, cs);
+        BufferedReader br = new BufferedReader(isr);
+        String line;
+
+        while((line = br.readLine()) != null){
+            totalNumberOfScratchCards += appendScratchCards(copiedScratchCards, currentGameNumber++, line);
+        }
+        br.close();
+
+        return totalNumberOfScratchCards;
+    }
+
+    /*
+     * Example input: 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+     */
+    private static int appendScratchCards(HashMap<Integer, Integer> copiedScratchCards, int currentGameNumber, String scratchCardLine) {
+        /*
+         * Results in:
+         * 'Card 1'
+         * ' 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
+         */
+        String[] scratchCardLineSplitOnColon = scratchCardLine.split(":");
+        /*
+         * Results in:
+         * ' 41 48 83 86 17 '
+         * ' 83 86  6 31 17  9 48 53'
+         */
+        String[] scratchCardLineSplitOnPipe = scratchCardLineSplitOnColon[1].split("\\|");
+
+        Integer[] winningNumbers = convertToNumberArray(scratchCardLineSplitOnPipe[0]);
+        Integer[] allNumbers = convertToNumberArray(scratchCardLineSplitOnPipe[1]);
+
+        HashSet<Integer> set = new HashSet<>();
+        set.addAll(Arrays.asList(winningNumbers));
+        set.retainAll(Arrays.asList(allNumbers));
+
+        int numberOfScratchCards = 1;
+        if (copiedScratchCards.containsKey(currentGameNumber)) {
+            numberOfScratchCards += copiedScratchCards.get(currentGameNumber);
+        }
+
+        for (int j=0; j<numberOfScratchCards; j++) {
+            for (int i=0; i<set.size(); i++) {
+                int gameNumber = currentGameNumber + i + 1;
+                int count = 1;
+                if (copiedScratchCards.containsKey(gameNumber)) {
+                    count = copiedScratchCards.get(gameNumber) + 1;
+                }
+
+                copiedScratchCards.put(gameNumber, count);
+            }
+        }
+
+        return numberOfScratchCards;
+    }
+
+    /*
+     * Example input: ' 83 86  6 31 17  9 48 53'
+     */
+    private static Integer[] convertToNumberArray(String numbers) {
+        // use trim to remove spaces on beginning/end, replace duplicate space in between
+        String [] str = numbers.trim().replaceAll("  ", " ").split(" ");
+        int size = str.length;
+        Integer[] arr = new Integer[size];
+        for(int i=0; i<size; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        return arr;
+    }
+}
