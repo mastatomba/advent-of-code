ac4c463cc0edbedd8118a213d5ef0e76a7f2f40d
Tom Schoutens <tom.schoutens@gmail.com>
2024-12-13 21:31:33 +0100

    Finish day 13 part 2 of year 2024

--- python/2024/day-13-2024.py
+++ python/2024/day-13-2024.py
@@ -1,7 +1,7 @@
 import re
 
-file_path = '/home/tom/Projects/personal/advent-of-code/resources/input/2024/day13/input_story.txt'
-# file_path = '/home/tom/Projects/personal/advent-of-code/resources/input/2024/day13/input.txt'
+# file_path = '/home/tom/Projects/personal/advent-of-code/resources/input/2024/day13/input_story.txt'
+file_path = '/home/tom/Projects/personal/advent-of-code/resources/input/2024/day13/input.txt'
 
 BUTTON_A_TOKENS = 3
 BUTTON_B_TOKENS = 1
@@ -37,19 +37,29 @@ class Machine:
     def __str__(self):
         return f"Machine(button_a={self.button_a}, button_b={self.button_b}, prize={self.prize})"
     
+    def get_min_token_count_to_win_cramer_rule(self, increase_prize: int):
+        #https://en.wikipedia.org/wiki/Cramer%27s_rule
+        prize_x = self.prize.x + increase_prize
+        prize_y = self.prize.y + increase_prize
+        #A = (p_x*b_y - prize_y*b_x) / (a_x*b_y - a_y*b_x)
+        #B = (a_x*p_y - a_y*p_x)     / (a_x*b_y - a_y*b_x)
+        A = (prize_x*self.button_b.y - prize_y*self.button_b.x) / (self.button_a.x*self.button_b.y - self.button_a.y*self.button_b.x)
+        B = (self.button_a.x*prize_y - self.button_a.y*prize_x) / (self.button_a.x*self.button_b.y - self.button_a.y*self.button_b.x)
+        if (A.is_integer() and B.is_integer()):
+            token_count = (int(A) * BUTTON_A_TOKENS) + (int(B) * BUTTON_B_TOKENS)
+            # print(f"  Win combination found! Button a pressed {A} times and button b pressed {B} times. Token count: {token_count}")
+            return token_count
+        return -1
+
     def get_min_token_count_to_win(self, max_button_presses: int, increase_prize: int):
         min_token_count = -1
         prize_x = self.prize.x + increase_prize
         prize_y = self.prize.y + increase_prize
-        start_index = int(min(int(prize_x / (self.button_a.x+self.button_b.x)), int(prize_y / (self.button_a.y+self.button_b.y))) / 10)
-        if (start_index < 0):
-            start_index = 0
-        print(f"  start_index={start_index}")
-        for i in range(start_index, max_button_presses):
+        for i in range(max_button_presses):
             a_x = i * self.button_a.x
             a_y = i * self.button_a.y
             if a_x > prize_x or a_y > prize_y:
-                print(f"    Button a pushes are already over prize possibility: {i}/{a_x}/{a_y}/{prize_x}/{prize_y}")
+                # print(f"    Button a pushes are already over prize possibility: {i}/{a_x}/{a_y}/{prize_x}/{prize_y}")
                 break
             remainder_x = prize_x - a_x;
             if (remainder_x % self.button_b.x == 0):
@@ -58,21 +68,10 @@ class Machine:
                     b_y = j * self.button_b.y
                     if (a_y + b_y) == prize_y:
                         token_count = (i * BUTTON_A_TOKENS) + (j * BUTTON_B_TOKENS)
-                        print(f"  Win combination found! Button a pressed {i} times and button b pressed {j} times. Token count: {token_count}")
+                        # print(f"  Win combination found! Button a pressed {i} times and button b pressed {j} times. Token count: {token_count}")
                         if min_token_count == -1 or token_count < min_token_count:
                             min_token_count = token_count
 
-            # for j in range(max_button_presses):
-            #     b_x = j * self.button_b.x
-            #     b_y = j * self.button_b.y
-            #     if b_x > prize_x or b_y > prize_y:
-            #         # print(f"    Button b pushes are already over prize possibility: {j}/{b_x}/{b_y}")
-            #         break
-            #     if (a_x + b_x) == prize_x and (a_y + b_y) == prize_y:
-            #         token_count = (i * BUTTON_A_TOKENS) + (j * BUTTON_B_TOKENS)
-            #         print(f"  Win combination found! Button a pressed {i} times and button b pressed {j} times. Token count: {token_count}")
-            #         if min_token_count == -1 or token_count < min_token_count:
-            #             min_token_count = token_count
         return min_token_count
 
 def extract_numbers(line: str):
@@ -103,13 +102,13 @@ for i in range(0, len(lines), 4):
 
 sum1 = sum2 = 0
 for machine in machines:
-    print(machine)
+    # print(machine)
     count = machine.get_min_token_count_to_win(100, 0)
     if (count != -1):
         sum1 += count
-    # count = machine.get_min_token_count_to_win(1000000000000000, 10000000000000)
-    # if (count != -1):
-    #     sum2 += count
+    count = machine.get_min_token_count_to_win_cramer_rule(10000000000000)
+    if (count != -1):
+        sum2 += count
 
 print(f"Part 1: {sum1}")
 
